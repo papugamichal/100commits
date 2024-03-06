3258b3285bd625a61ed0e6774c683fdb0bf449aa
papugamichal <papuga.michal@hotmail.com>
2024-03-06 21:43:51 +0100

    Create test .proto and connect gRPC servcer with client.

--- src/gRPC.AirServer/Program.cs
+++ src/gRPC.AirServer/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using gRPC.Server.GrpcServices;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", context => context.Response.WriteAsJsonAsync("Hello world!"));
+
+app.Run();
--- src/gRPC.AirStation/Program.cs
+++ src/gRPC.AirStation/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Grpc.Net.Client;
+
 Console.WriteLine("Hello, World!");
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5555");
+await channel.ConnectAsync();
+
+var client = new Greeter.GreeterClient(channel);
+var response = await client.SayHelloAsync(new HelloRequest() { Name = "Ping" });
+
+Console.WriteLine("Greeting: " + response.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
