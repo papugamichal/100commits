86b16136960e3b9dbb7428bd78f0e905fb71fcae
papugamichal <papuga.michal@hotmail.com>
2024-03-07 17:16:19 +0100

    Define airQ .protos for data producent (Station) and consumers (Clients)

--- src/gRPC.AirStation/Program.cs
+++ src/gRPC.AirStation/Program.cs
@@ -11,6 +11,20 @@ await channel.ConnectAsync();
 var client = new Greeter.GreeterClient(channel);
 var response = await client.SayHelloAsync(new HelloRequest() { Name = "Ping" });
 
+
+var producer = new AirQProducer.AirQProducerClient(channel);
+var request = producer.StreamToServer();
+await request.RequestStream.WriteAsync(new AirData()
+{
+    StationName = "Krakow S1",
+    Metrics = new AirQMetrics()
+    {
+
+    }
+});
+await request.ResponseAsync;
+
+
 Console.WriteLine("Greeting: " + response.Message);
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
