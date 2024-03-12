8a8721216b0737c8338fd7fb86b53d90c54e8c6e
papugamichal <papuga.michal@hotmail.com>
2024-03-12 20:57:30 +0100

    Bidirection streaming - draft

--- src/gRPC.AirServer/GrpcServices/GreeterService.cs
+++ src/gRPC.AirServer/GrpcServices/GreeterService.cs
@@ -41,12 +41,44 @@ public class ConsumerService : AirQConsumer.AirQConsumerBase
         }
     }
 
-    public override async Task StreamUpdates2(IAsyncStreamReader<StreamRequest> requestStream, IServerStreamWriter<AirQMetrics> responseStream, ServerCallContext context)
+    public override async Task GetBidirectionalUpdatesStream(IAsyncStreamReader<StreamRequest> requestStream, IServerStreamWriter<AirQMetrics> responseStream, ServerCallContext context)
     {
         var reqId = context.GetHttpContext().TraceIdentifier;
         
-        await responseStream.WriteAsync(new AirQMetrics());
+        Console.WriteLine($"Request `{nameof(GetBidirectionalUpdatesStream)}`, id: `{reqId}`.");
+        var requestArg = requestStream.Current?.StationName ?? string.Empty;
+        
+        var t1 = Task.Run(async () =>
+        {
+            await foreach (var item in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                requestArg = item.StationName;
+                
+                Console.WriteLine($"Request `{nameof(GetBidirectionalUpdatesStream)}`, id: `{reqId}`. New arg: {requestArg}");
+            }
+        });
 
+        var t2 = Task.Run(async () =>
+        {
+            var random = new Random();
+            while (context.CancellationToken.IsCancellationRequested == false)
+            {
+                var data = new AirQMetrics
+                {
+                    Humidity = random.NextDouble() * 10,
+                    Pm10 = (int) random.NextInt64(0, 150),
+                    No2 = (int) random.NextInt64(0, 150),
+                    So2 = (int) random.NextInt64(0, 150),
+                    Pressure = (int) random.NextInt64(990, 1050),
+                    Temperature = random.NextInt64(-15, 12) + random.NextDouble()
+                };
+                await responseStream.WriteAsync(data);
+                await Task.Delay(TimeSpan.FromSeconds(2), context.CancellationToken);
+            }
+        });
+
+        await Task.WhenAll(t1, t2);
+        Console.WriteLine($"Request `{nameof(GetBidirectionalUpdatesStream)}` finished, id: `{reqId}`.");
     }
 }
 
