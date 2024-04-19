6481a52c70cf7ca986b7812095c191e986d057fd
papugamichal <papuga.michal@hotmail.com>
2024-04-19 20:19:26 +0200

    BlazorEventBus - in-memory (on channels) - start working on subscriptions model

--- src/BlazorEventBus/BlazorEventBus/InMemory/InMemoryChannelBEventBus.cs
+++ src/BlazorEventBus/BlazorEventBus/InMemory/InMemoryChannelBEventBus.cs
@@ -1,4 +1,6 @@
+using System.Threading.Channels;
 using BlazorEventBus.Abstractions;
+using JetBrains.Annotations;
 
 namespace BlazorEventBus.InMemory;
 
@@ -14,7 +16,53 @@ internal class InMemoryChannelBEventBus : IBEventBus
         throw new NotImplementedException();
     }
 
+    [MustDisposeResource]
     public IDisposable Subscribe<TEvent>(Func<TEvent, Task> handler) where TEvent : class, IEvent
+    {
+        return new Subscription<TEvent>(this, handler);
+    }
+    
+    [MustDisposeResource]
+    private sealed class Subscription<TEvent> : IDisposable where TEvent : IEvent
+    {
+        private readonly Func<TEvent, Task> _handler;
+        private Func<TEvent, Task> handler;
+        
+        public Subscription(InMemoryChannelBEventBus clazz, Func<TEvent, Task> handler)
+        {
+            _handler = handler;
+            
+            var channel = Channel.CreateBounded<TEvent>(1);
+            var t = Task.Run(async () =>
+            {
+                await foreach (var @event in channel.Reader.ReadAllAsync(CancellationToken.None))
+                {
+                    await handler(@event);
+                };
+            });
+
+            clazz.RegisterSubscription<TEvent>(this);
+
+        }
+        private void ReleaseUnmanagedResources()
+        {
+            // TODO release unmanaged resources here
+        }
+
+        [HandlesResourceDisposal]
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~Subscription()
+        {
+            ReleaseUnmanagedResources();
+        }
+    }
+
+    private void RegisterSubscription<TEvent>(Subscription<TEvent> subscription) where TEvent : IEvent
     {
         throw new NotImplementedException();
     }
